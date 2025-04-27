199700667f9e5afd0bb9bd825103bc6b762b2cff
K4p4h4l4 <tolavio@hotmail.com>
2025-04-27 20:13:36 +0100

    Update and rename listar_processos.ps1 to listar_processos.yml

--- listar_processos.ps1
+++
@@ -1,5 +0,0 @@
-# Listar processos em execução, mostrando Nome, ID, CPU e Memória (WS)
-Get-Process |
-    Select-Object Name, Id, CPU, WS |
-    Sort-Object CPU -Descending |
-    Format-Table -AutoSize
